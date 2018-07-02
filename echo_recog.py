1e7ba96102a86f5b8a85fe5217f41d71a01ada06
haoqizhenhao <34744138+haoqizhenhao@users.noreply.github.com>
2018-07-03 00:22:32 +0800

    echo_recog
    tensorflow

---
+++ echo_recog.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 19:48:00 2018
+模式识别算法：
+深海采矿时，经常用超声探测待开采区的底质，本算法用于识别混响环境下的钴结壳底质、基岩（玄武岩、花岗岩等），泥沙底质
+@author: haoqi
+"""
+
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+
+
